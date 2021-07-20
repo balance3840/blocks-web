10d9b68fa7f3c29b48a1c0529f30348f05fde416
Ramiro Estrella <raest@gramjuhl.com>
2021-07-20 15:58:03 +0200

    Adding stream

--- src/env.example.js
+++ src/env.example.js
@@ -4,4 +4,4 @@ const PORT = '8080';
 const API_PATH = '/api';
 
 export const API_URL = `${PROTOCOL}://${HOST}:${PORT}${API_PATH}`;
-export const STREAM_URL = `${PROTOCOL}://${HOST}:8085/hls/5e339f47264ff3ab466e8574b48cc.m3u8`;
+export const STREAM_URL = `${PROTOCOL}://${HOST}:8085/hls/5e339f47.m3u8`;
